a7f21b849479cdda45fbcc1749f15116374657a0
Bharanidharan <bharanidharan.m@hotmail.com>
2017-11-09 16:15:35 +0000

    test

--- src/main/java/com/codenvy/template/android/GetTask.java
+++ src/main/java/com/codenvy/template/android/GetTask.java
@@ -6,44 +6,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.io.IOException;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.widget.TextView;
+import java.net.MalformedURLException;
+import java.io.InputStream;
+
 public class GetTask extends AsyncTask<Void, Void, Void> 
 {
-    String returnMessage = "";
+    String returnMessage = "initial message";
     
     @Override
     protected Void doInBackground(Void... params) 
     {
         try
         {
-            String urlStr = "http://test-2956.apphb.com/api/Values/login";
-            HttpURLConnection urlConnection = null;
-            URL url = new URL(urlStr);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setReadTimeout(10000 /* milliseconds */ );
-            urlConnection.setConnectTimeout(15000 /* milliseconds */ );
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuilder sb = new StringBuilder();
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            
-            br.close();
-
-            returnMessage = sb.toString();
+        	String urlStr = "http://test-2956.apphb.com/api/Values/login";
+          
+          	URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            InputStream stream = conn.getInputStream();
+          	BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+          
+          	String line = "";
+          
+          	while (line != null)
+          	{
+            	line = reader.readLine();
+            	returnMessage = returnMessage + line;
+          	}
         }
-        catch(IOException ex)
+        catch(MalformedURLException e)
         {
-            throw new RuntimeException(ex);
+          e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+          e.printStackTrace();
         }
         
         return null;
